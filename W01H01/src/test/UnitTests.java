cffc78b9ea5966bc2dba93d8c70a6a1ed8d9307a
Jonas Ladner <jonas.ladner@tum.de>
2021-11-05 21:16:42 +0100

    Added UnitTests for W01H01

---
+++ W01H01/src/test/UnitTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Test;
+import org.junit.Assert;
+import pgdp.hellopingu.Hello;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class UnitTests {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    @Test
+    public void checkMain() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Hello.main(new String[]{});
+        Assert.assertEquals("Hello World\r\nSüßer Pingu!", outputStreamCaptor.toString()
+                .trim());
+        System.setOut(standardOut);
+    }
+}
