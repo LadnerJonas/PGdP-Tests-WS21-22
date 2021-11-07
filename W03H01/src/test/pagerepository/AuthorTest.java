cf4544ba9d7dc81ffc06fddb3108e55de79a4cc0
Paul Zhang <me@paul-zhang.de>
2021-11-07 20:58:58 +0100

    Add tests for W03H01

---
+++ W03H01/src/test/pagerepository/AuthorTest.java
@@ -0,0 +1,55 @@
+package pgdp.test.pagerepository;
+
+import org.junit.jupiter.api.Test;
+import pgdp.searchengine.pagerepository.Author;
+import pgdp.searchengine.util.Date;
+
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AuthorTest {
+
+    private static final Author AUTHOR = new Author("Max", "Mustermann", "Pinguinstraße 1",
+            "max.mustermann@example.de", new Date(12, 10, 1990));
+
+    @Test
+    void testConstructor() {
+        assertEquals("Max", AUTHOR.getFirstName());
+        assertEquals("Mustermann", AUTHOR.getLastName());
+        assertEquals("Pinguinstraße 1", AUTHOR.getAddress());
+        assertEquals("max.mustermann@example.de", AUTHOR.getEmail());
+        assertTrue(new Date(12, 10, 1990).equals(AUTHOR.getBirthday()));
+    }
+
+    @Test
+    void testToPrintText() {
+        assertTrue(Pattern.compile("""
+                ^(?=.*\\bMax\\b)(?=.*\\bMustermann\\b).*
+                (?=\\b.*12\\b)(?=\\b.*10\\b)(?=\\b.*1990\\b).*
+                .*Pinguinstraße 1.*
+                .*max\\.mustermann@example\\.de.*$""").matcher(AUTHOR.toPrintText()).matches());
+    }
+
+    @Test
+    void testEquals() {
+        assertTrue(AUTHOR.equals(AUTHOR));
+        assertTrue(AUTHOR.equals(new Author("Max", "Mustermann", "Pinguinstraße 1",
+                "max.mustermann@example.de", new Date(12, 10, 1990))));
+        assertFalse(AUTHOR.equals(new Author("Max", "Mustermann", "Pinguinstraße 1",
+                "max.mustermann@example.de", new Date(12, 10, 1989))));
+        assertFalse(AUTHOR.equals(new Author("Jonas", "Mustermann", "Pinguinstraße 1",
+                "max.mustermann@example.de", new Date(12, 10, 1990))));
+        assertFalse(AUTHOR.equals(new Author("Max", "Musterfrau", "Pinguinstraße 1",
+                "max.mustermann@example.de", new Date(12, 10, 1990))));
+        assertFalse(AUTHOR.equals(new Author("Max", "Mustermann", "Pinguinstraße 1",
+                "max.mustermann@example.de", null)));
+    }
+
+    @Test
+    void testToString() {
+        assertTrue(Pattern.compile("^(?=.*\\bMax\\b)(?=.*\\bMustermann\\b).*").matcher(AUTHOR.toString()).matches());
+    }
+
+
+}
---
+++ W03H01/src/test/pagerepository/DocumentTest.java
@@ -0,0 +1,57 @@
+package pgdp.test.pagerepository;
+
+import org.junit.jupiter.api.Test;
+import pgdp.searchengine.pagerepository.Author;
+import pgdp.searchengine.pagerepository.Document;
+import pgdp.searchengine.util.Date;
+
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DocumentTest {
+
+    @Test
+    void testConstructor() {
+        int initial = Document.numberOfCreatedDocuments();
+        var document = document();
+        assertEquals(initial + 1, Document.numberOfCreatedDocuments());
+        assertEquals(initial, document.getDocumentId());
+        var document1 = document();
+        assertEquals(initial + 2, Document.numberOfCreatedDocuments());
+        assertEquals(initial + 1, document1.getDocumentId());
+
+        assertTrue(new Date(12, 11, 1990).equals(document.getReleaseDate()));
+        assertTrue(new Date(12, 11, 1990).equals(document.getLastUpdateDate()));
+    }
+
+    @Test
+    void testToPrintText() {
+        assertTrue(Pattern.compile("""
+                ^.*Musterdokument.*
+                (?=.*\\bMax\\b)(?=.*\\bMustermann\\b).*
+                .*Mustertext.*
+                (?=\\b.*12\\b)(?=\\b.*11\\b)(?=\\b.*1990\\b).*$""").matcher(document().toPrintText()).matches());
+    }
+
+    @Test
+    void testEquals() {
+        var document = document();
+        assertTrue(document.equals(document));
+
+        var document2 = document();
+        assertFalse(document2.equals(document));
+    }
+
+    @Test
+    void testToString() {
+        var pattern = Pattern.compile("^(?=.*\\bMusterdokument\\b)(?=.*12)(?=.*11)(?=.*1990)(?=.*\\bMax\\b)(?=.*\\bMustermann).*$");
+        assertTrue(pattern.matcher(document().toString()).matches());
+    }
+
+    private Document document() {
+        var author = new Author("Max", "Mustermann", "Pinguinstraße 1", "test@example.de", new Date(1, 1, 1990));
+        return new Document("Musterdokument", "Mustertext", "Lorem Ipsum dolor sit amet, consectetur adipiscing elit", new Date(12, 11, 1990), author);
+    }
+
+}
