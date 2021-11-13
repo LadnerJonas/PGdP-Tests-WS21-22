e69595a272c4155c726593509efcae84b199dcb6
Pat396 <74953132+Pat396@users.noreply.github.com>
2021-11-13 15:09:20 +0100

    added tests for W04H01

---
+++ W04H01/src/UnitTests/TestNumberConverter.java
@@ -0,0 +1,80 @@
+package UnitTest;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static pgdp.pingumath.NumberConverter.intToPinguNum;
+import static pgdp.pingumath.NumberConverter.pinguNumToInt;
+
+public class TestNumberConverter {
+
+    @Test
+    void testIntToPinguNegative(){
+        String s = intToPinguNum(-2);
+        assertEquals("N.D.",s);
+
+    }
+
+    @ParameterizedTest
+    @MethodSource ("provideParametersIntToNum")
+    //checking multiple values for IntToPingu
+    void testIntToPingu(String expectedPinguNum, int decimalNum){
+        String s = intToPinguNum(decimalNum);
+        assertEquals(expectedPinguNum,s);
+
+    }
+    private static Stream<Arguments> provideParametersIntToNum() {
+        return Stream.of(
+                Arguments.of("In", 0),
+                Arguments.of("Gu", 1),
+                Arguments.of("Pin", 2),
+                Arguments.of("Pinguin", 21),
+                Arguments.of("Gugupin", 14),
+                Arguments.of("Gupinpinguin", 156),
+                Arguments.of("Gupinpinguininingupinin", 37923)
+        );
+    }
+
+    //Checking multiple invalid values
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "pinguin", "Piniguin", "abc"})
+    void testNumToIntInvalid(String string){
+        int val = pinguNumToInt(string);
+        assertEquals(-1, val);
+
+    }
+    //Testing multiple values for PinguToInt
+    @ParameterizedTest
+    @MethodSource("provideParametersNumToInt")
+    void testNumToIntValid(String num, int expected_val){
+        int val = pinguNumToInt(num);
+        assertEquals(expected_val, val);
+
+    }
+    private static Stream<Arguments> provideParametersNumToInt() {
+        return Stream.of(
+                Arguments.of("In", 0),
+                Arguments.of("Gu", 1),
+                Arguments.of("Pin", 2),
+                Arguments.of("Pinguin", 21),
+                Arguments.of("Gugupin", 14),
+                Arguments.of("Gupinpinguin", 156),
+                Arguments.of("Gupinpinguininingupinin",37923)
+        );
+
+    }
+}
+
+
+
+
+
+
