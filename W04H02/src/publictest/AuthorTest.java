76890f7d13e1983ced25dab1ddd17ec066f00081
Jonas Ladner <jonas.ladner@tum.de>
2021-11-18 22:18:29 +0100

    W04H02 AuthorTest: Add some public Tests, no homework sorry

---
+++ W04H02/src/publictest/AuthorTest.java
@@ -0,0 +1,23 @@
+package publictest;
+
+import org.junit.jupiter.api.Test;
+import pgdp.searchengine.pagerepository.Author;
+import pgdp.searchengine.util.Date;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AuthorTest {
+  @Test
+  public void getAgeTest() {
+    Author author1 = new Author("Jonas", "Ladner", "Garching-City", "tum@tum.de", new Date(27, 11, 2000));
+    assertEquals(20, author1.getAge());
+    
+    Author author2 = new Author("Max", "Mustermann", "Garching-City", "tum@tum.de", new Date(LocalDate.now().getDayOfMonth(), LocalDate.now().getMonthValue(), LocalDate.now().getYear()));
+    assertEquals(0, author2.getAge());
+    
+    Author author3 = new Author("Erika", "Mustermann", "Garching-City", "tum@tum.de", new Date(27, 1, 2000));
+    assertEquals(21, author3.getAge());
+  }
+}
