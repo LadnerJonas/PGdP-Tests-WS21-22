765c50ce7741d5a9631ad646e30c613fc8283ebf
Richard <strunk876@gmail.com>
2021-12-04 10:40:03 +0100

    Fixed lineSeparator and added "food overflow" test

--- W06H01/src/test/PinguFoodLogisticsTest.java
+++ W06H01/src/test/PinguFoodLogisticsTest.java
@@ -14,7 +14,7 @@ import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /*
  If you think a test is wrong, please open an Issue on GitHub.
@@ -23,117 +23,131 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class PinguFoodLogisticsTest {
+  private final static String lineSeparator = System.lineSeparator();
   private PrintStream old;
   private ByteArrayOutputStream baos = new ByteArrayOutputStream();
-  
+
   @BeforeEach
-  void ResetRandomSeed() {
+  void resetSeed() {
     // Using Reflection to access private fields outside of class.
     // (Psst! Do not tell the so-called Übungsleitung)
-    PinguLib pinguLib = new PinguLib();
-    Class obj = pinguLib.getClass();
     try {
-      Field field = obj.getDeclaredField("rand");
+      Field field = PinguLib.class.getDeclaredField("rand");
       field.setAccessible(true);
-      field.set(pinguLib, null);
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      e.printStackTrace();
+      field.set(null/* field is static */, null);
+    } catch (NoSuchFieldException | IllegalAccessException exception) {
+      exception.printStackTrace();
     }
     PinguLib.setRandom();
   }
-  
+
   @BeforeEach
-  void PrepareConsole() {
+  void prepareConsole() {
     baos = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(baos);
     System.setOut(ps);
   }
-  
+
+  void resetConsole() {
+    System.out.flush();
+  }
+
   @AfterEach
-  void ResetConsole() {
+  void unsetConsole() {
     System.out.flush();
     System.setOut(old);
   }
-  
+
   @Test
   void registerUnusedFoodAndprintWasteStatisticsTest() {
     PinguFoodLogistics pfl1 = new PinguFoodLogistics(BigDecimal.valueOf(5), BigDecimal.valueOf(10), BigDecimal.valueOf(15));
     pfl1.printWasteStatistics();
-    
-    assertTrue(baos.toString().contains("Bisher konnten 0 Tiere mit einem Gesamtgewicht von 0g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 0PD entgangen."));
-    
+
+    assertContains(baos, "Bisher konnten 0 Tiere mit einem Gesamtgewicht von 0g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 0PD entgangen.");
     pfl1.acceptNewOrder(new AmountOrder(1, 2, 3));
     pfl1.clearOrderBook();
-    PrepareConsole();
+    resetConsole();
     pfl1.printWasteStatistics();
-    
+
     //assertEquals("", baos.toString()); can be used to check what your console output is
-    assertTrue(baos.toString().contains("Bisher konnten 15 Tiere mit einem Gesamtgewicht von 2373g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 35595PD entgangen."));
-    
+    assertContains(baos, "Bisher konnten 15 Tiere mit einem Gesamtgewicht von 2373g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 35595PD entgangen.");
+
     pfl1.acceptNewOrder(new WeightOrder(1000));
     pfl1.clearOrderBook();
-    PrepareConsole();
+    resetConsole();
     pfl1.printWasteStatistics();
-    
-    assertTrue(baos.toString().contains("Bisher konnten 37 Tiere mit einem Gesamtgewicht von 4959g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 73735PD entgangen."));
-    
+
+    assertContains(baos, "Bisher konnten 37 Tiere mit einem Gesamtgewicht von 4959g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 73735PD entgangen.");
+
     pfl1.acceptNewOrder(new TradeOrder());
     pfl1.clearOrderBook();
-    PrepareConsole();
+    resetConsole();
     pfl1.printWasteStatistics();
-    
-    assertTrue(baos.toString().contains("Bisher konnten 38 Tiere mit einem Gesamtgewicht von 5055g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 75175PD entgangen."));
+
+    assertContains(baos, "Bisher konnten 38 Tiere mit einem Gesamtgewicht von 5055g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 75175PD entgangen.");
   }
-  
+
   @Test
   void clearOrderBookTest() {
     PinguFoodLogistics pfl1 = new PinguFoodLogistics(BigDecimal.valueOf(5), BigDecimal.valueOf(10), BigDecimal.valueOf(15));
     pfl1.clearOrderBook();
-    
+
     //assertEquals("", baos.toString()); can be used to check what your console output is
-    assertTrue(baos.toString().contains("Es können 0 Bestellungen abgearbeitet werden."));
-    PrepareConsole();
-    
+    assertContains(baos, "Es können 0 Bestellungen abgearbeitet werden.");
+    resetConsole();
+
     pfl1.acceptNewOrder(new AmountOrder(3, 0, 5));
     pfl1.clearOrderBook();
-    
-    assertTrue(baos.toString().contains("Es können 1 Bestellungen abgearbeitet werden."));
-    assertTrue(baos.toString().contains("Die Bestellung(Anzahl: [3,0,5]) hat ein Gesamtgewicht von 1107g und kostet 15685PD."));
-    PrepareConsole();
+
+    assertContains(baos, "Es können 1 Bestellungen abgearbeitet werden.");
+    assertContains(baos, "Die Bestellung(Anzahl: [3,0,5]) hat ein Gesamtgewicht von 1107g und kostet 15685PD.");
+    resetConsole();
     pfl1.printWasteStatistics();
-    assertTrue(baos.toString().contains("Bisher konnten 17 Tiere mit einem Gesamtgewicht von 2289g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 33525PD entgangen."));
-    PrepareConsole();
-    
+    assertContains(baos, "Bisher konnten 17 Tiere mit einem Gesamtgewicht von 2289g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 33525PD entgangen.");
+    resetConsole();
+
     pfl1.acceptNewOrder(new TradeOrder());
     pfl1.acceptNewOrder(new WeightOrder(0));
     pfl1.acceptNewOrder(new AmountOrder(0, 0, 0));
     pfl1.clearOrderBook();
-    assertTrue(baos.toString().contains("Es können 3 Bestellungen abgearbeitet werden."));
-    assertTrue(baos.toString().contains("Die Bestellung(Einzeln) hat ein Gesamtgewicht von 2g und kostet 20PD."));
-    PrepareConsole();
+    assertContains(baos, "Es können 3 Bestellungen abgearbeitet werden.");
+    assertContains(baos, "Die Bestellung(Einzeln) hat ein Gesamtgewicht von 2g und kostet 20PD.");
+    resetConsole();
     pfl1.printWasteStatistics();
-    assertTrue(baos.toString().contains("Bisher konnten 17 Tiere mit einem Gesamtgewicht von 2289g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 33525PD entgangen."));
-    
+    assertContains(baos, "Bisher konnten 17 Tiere mit einem Gesamtgewicht von 2289g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 33525PD entgangen.");
+
     pfl1.acceptNewOrder(new TradeOrder());
     pfl1.acceptNewOrder(new WeightOrder(500));
     pfl1.acceptNewOrder(new AmountOrder(9, 7, 8));
     pfl1.clearOrderBook();
-    assertTrue(baos.toString().contains("Es können 3 Bestellungen abgearbeitet werden."));
-    assertTrue(baos.toString().contains("Die Bestellung(Einzeln) hat ein Gesamtgewicht von 10g und kostet 100PD."));
-    assertTrue(baos.toString().contains("Die Bestellung(Zielgewicht: 500g) hat ein Gesamtgewicht von 597g und kostet 7180PD."));
-    assertTrue(baos.toString().contains("Die Bestellung(Anzahl: [9,7,8]) hat ein Gesamtgewicht von 2029g und kostet 27340PD."));
-    PrepareConsole();
+    assertContains(baos, "Es können 3 Bestellungen abgearbeitet werden.");
+    assertContains(baos, "Die Bestellung(Einzeln) hat ein Gesamtgewicht von 10g und kostet 100PD.");
+    assertContains(baos, "Die Bestellung(Zielgewicht: 500g) hat ein Gesamtgewicht von 597g und kostet 7180PD.");
+    assertContains(baos, "Die Bestellung(Anzahl: [9,7,8]) hat ein Gesamtgewicht von 2029g und kostet 27340PD.");
+    resetConsole();
     pfl1.printWasteStatistics();
-    assertTrue(baos.toString().contains("Bisher konnten 32 Tiere mit einem Gesamtgewicht von 4452g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 65510PD entgangen."));
-    
+    assertContains(baos, "Bisher konnten 32 Tiere mit einem Gesamtgewicht von 4452g nicht verwertet werden." + lineSeparator +
+      "Claudia und Karl-Heinz ist dadurch ein Profit von 65510PD entgangen.");
+
   }
-  
-  
+
+  private void assertContains(ByteArrayOutputStream baos, String needle) {
+    String haystack = baos.toString();
+    if (!haystack.contains(needle)) {
+      fail("\"%s\" (length %d) does not contain \"%s\"(length %d) "
+        .formatted(
+          haystack.replace("\n", "\\n").replace("\r", "\\r"),
+          haystack.length(),
+          needle.replace("\n", "\\n").replace("\r", "\\r"),
+          needle.length()
+        )
+      );
+    }
+  }
 }
