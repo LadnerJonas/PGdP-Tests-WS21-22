45c3b1e06719454add4d2f16dbf51d52a4c2ae27
Jonas Ladner <jonas.ladner@tum.de>
2021-12-13 13:58:44 +0100

    Fix W06H01 changes

--- W06H01/src/test/PinguFoodLogisticsTest.java
+++ W06H01/src/test/PinguFoodLogisticsTest.java
@@ -1,30 +1,21 @@
 package test;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import pgdp.PinguLib;
-import pgdp.saleuine2.AmountOrder;
-import pgdp.saleuine2.PinguFoodLogistics;
-import pgdp.saleuine2.TradeOrder;
-import pgdp.saleuine2.WeightOrder;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /*
  If you think a test is wrong, please open an Issue on GitHub.
  See https://github.com/LadnerJonas/PGdP-Tests-WS21-22#important-note-1
  */
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pgdp.PinguLib;
+import pgdp.saleuine2.*;
 
-public class PinguFoodLogisticsTest {
-  private PrintStream old;
-  private ByteArrayOutputStream baos = new ByteArrayOutputStream();
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TradeOrderAndHeirsTest {
   
   @BeforeEach
   void ResetRandomSeed() {
@@ -42,98 +33,146 @@ public class PinguFoodLogisticsTest {
     PinguLib.setRandom();
   }
   
-  @BeforeEach
-  void PrepareConsole() {
-    baos = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(baos);
-    System.setOut(ps);
-  }
-  
-  @AfterEach
-  void ResetConsole() {
-    System.out.flush();
-    System.setOut(old);
+  // TradeOrder
+  @Test
+  void orderTypeTradeOrder() {
+    TradeOrder to1 = new TradeOrder();
+    
+    assertEquals("Einzeln", to1.orderType());
   }
   
   @Test
-  void registerUnusedFoodAndprintWasteStatisticsTest() {
-    PinguFoodLogistics pfl1 = new PinguFoodLogistics(BigDecimal.valueOf(5), BigDecimal.valueOf(10), BigDecimal.valueOf(15));
-    pfl1.printWasteStatistics();
+  void toStringTradeOrder() {
+    TradeOrder to1 = new TradeOrder();
     
-    assertTrue(baos.toString().contains("Bisher konnten 0 Tiere mit einem Gesamtgewicht von 0g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 0PD entgangen."));
-    
-    pfl1.acceptNewOrder(new AmountOrder(1, 2, 3));
-    pfl1.clearOrderBook();
-    PrepareConsole();
-    pfl1.printWasteStatistics();
-    
-    //assertEquals("", baos.toString()); can be used to check what your console output is
-    assertTrue(baos.toString().contains("Bisher konnten 15 Tiere mit einem Gesamtgewicht von 2373g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 35595PD entgangen."));
-    
-    pfl1.acceptNewOrder(new WeightOrder(1000));
-    pfl1.clearOrderBook();
-    PrepareConsole();
-    pfl1.printWasteStatistics();
-    
-    assertTrue(baos.toString().contains("Bisher konnten 37 Tiere mit einem Gesamtgewicht von 4959g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 73735PD entgangen."));
-    
-    pfl1.acceptNewOrder(new TradeOrder());
-    pfl1.clearOrderBook();
-    PrepareConsole();
-    pfl1.printWasteStatistics();
-    
-    assertTrue(baos.toString().contains("Bisher konnten 38 Tiere mit einem Gesamtgewicht von 5055g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 75175PD entgangen."));
+    assertEquals("Die Bestellung(Einzeln) hat ein Gesamtgewicht von 0g und kostet 0PD.", to1.toString());
+    to1.supplyOrder(new Anchovie(100, 200), BigDecimal.ONE);
+    assertEquals("Die Bestellung(Einzeln) hat ein Gesamtgewicht von 200g und kostet 1PD.", to1.toString());
   }
   
   @Test
-  void clearOrderBookTest() {
-    PinguFoodLogistics pfl1 = new PinguFoodLogistics(BigDecimal.valueOf(5), BigDecimal.valueOf(10), BigDecimal.valueOf(15));
-    pfl1.clearOrderBook();
+  void isOrderFulfilledTradeOrder() {
+    TradeOrder to1 = new TradeOrder();
     
-    //assertEquals("", baos.toString()); can be used to check what your console output is
-    assertTrue(baos.toString().contains("Es können 0 Bestellungen abgearbeitet werden."));
-    PrepareConsole();
+    assertFalse(to1.isOrderFulfilled());
     
-    pfl1.acceptNewOrder(new AmountOrder(3, 0, 5));
-    pfl1.clearOrderBook();
-    
-    assertTrue(baos.toString().contains("Es können 1 Bestellungen abgearbeitet werden."));
-    assertTrue(baos.toString().contains("Die Bestellung(Anzahl: [3,0,5]) hat ein Gesamtgewicht von 1107g und kostet 15685PD."));
-    PrepareConsole();
-    pfl1.printWasteStatistics();
-    assertTrue(baos.toString().contains("Bisher konnten 17 Tiere mit einem Gesamtgewicht von 2289g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 33525PD entgangen."));
-    PrepareConsole();
-    
-    pfl1.acceptNewOrder(new TradeOrder());
-    pfl1.acceptNewOrder(new WeightOrder(0));
-    pfl1.acceptNewOrder(new AmountOrder(0, 0, 0));
-    pfl1.clearOrderBook();
-    assertTrue(baos.toString().contains("Es können 3 Bestellungen abgearbeitet werden."));
-    assertTrue(baos.toString().contains("Die Bestellung(Einzeln) hat ein Gesamtgewicht von 2g und kostet 20PD."));
-    PrepareConsole();
-    pfl1.printWasteStatistics();
-    assertTrue(baos.toString().contains("Bisher konnten 17 Tiere mit einem Gesamtgewicht von 2289g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 33525PD entgangen."));
-    
-    pfl1.acceptNewOrder(new TradeOrder());
-    pfl1.acceptNewOrder(new WeightOrder(500));
-    pfl1.acceptNewOrder(new AmountOrder(9, 7, 8));
-    pfl1.clearOrderBook();
-    assertTrue(baos.toString().contains("Es können 3 Bestellungen abgearbeitet werden."));
-    assertTrue(baos.toString().contains("Die Bestellung(Einzeln) hat ein Gesamtgewicht von 10g und kostet 100PD."));
-    assertTrue(baos.toString().contains("Die Bestellung(Zielgewicht: 500g) hat ein Gesamtgewicht von 597g und kostet 7180PD."));
-    assertTrue(baos.toString().contains("Die Bestellung(Anzahl: [9,7,8]) hat ein Gesamtgewicht von 2029g und kostet 27340PD."));
-    PrepareConsole();
-    pfl1.printWasteStatistics();
-    assertTrue(baos.toString().contains("Bisher konnten 32 Tiere mit einem Gesamtgewicht von 4452g nicht verwertet werden.\n" +
-            "Claudia und Karl-Heinz ist dadurch ein Profit von 65510PD entgangen."));
-    
+    to1.supplyOrder(new Anchovie(100, 200), BigDecimal.ONE);
+    assertTrue(to1.isOrderFulfilled());
   }
   
+  @Test
+  void supplyOrderTradeOrder() {
+    TradeOrder to1 = new TradeOrder();
+    
+    assertFalse(to1.supplyOrder(new PinguFood(1, 100), BigDecimal.ONE));
+    assertTrue(to1.supplyOrder(new Anchovie(1, 100), BigDecimal.ONE));
+    assertFalse(to1.supplyOrder(new Anchovie(1, -100), BigDecimal.ONE));
+    assertTrue(to1.isOrderFulfilled());
+  }
   
+  // WeightOrder
+  @Test
+  void orderTypeWeightOrder() {
+    TradeOrder to1 = new WeightOrder(100);
+    
+    assertEquals("Zielgewicht: 100g", to1.orderType());
+  }
+  
+  @Test
+  void toStringWeightOrder() {
+    TradeOrder to1 = new WeightOrder(100);
+    
+    assertEquals("Die Bestellung(Zielgewicht: 100g) hat ein Gesamtgewicht von 0g und kostet 0PD.", to1.toString());
+    
+    to1.supplyOrder(new Anchovie(100, 200), BigDecimal.ONE);
+    assertEquals("Die Bestellung(Zielgewicht: 100g) hat ein Gesamtgewicht von 200g und kostet 1PD.", to1.toString());
+  }
+  
+  @Test
+  void isOrderFulfilledWeightOrder() {
+    TradeOrder to1 = new WeightOrder(100);
+    
+    assertFalse(to1.isOrderFulfilled());
+    
+    to1.supplyOrder(new Anchovie(100, 200), BigDecimal.ONE);
+    assertTrue(to1.isOrderFulfilled());
+  }
+  
+  @Test
+  void supplyOrderWeightOrder() {
+    TradeOrder to1 = new WeightOrder(100);
+    
+    assertFalse(to1.supplyOrder(new PinguFood(1, 100), BigDecimal.ONE));
+    assertTrue(to1.supplyOrder(new Anchovie(1, 50), BigDecimal.ONE));
+    assertFalse(to1.isOrderFulfilled());
+    assertTrue(to1.supplyOrder(new Anchovie(1, 50), BigDecimal.ONE));
+    assertFalse(to1.supplyOrder(new Anchovie(1, -100), BigDecimal.ONE));
+    assertTrue(to1.isOrderFulfilled());
+  }
+  
+  // AmountOrder
+  @Test
+  void orderTypeAmountOrder() {
+    TradeOrder to1 = new AmountOrder(59, 75, 83);
+    
+    assertEquals("Anzahl: [59,75,83]", to1.orderType());
+  }
+  
+  @Test
+  void toStringAmountOrder() {
+    TradeOrder to1 = new AmountOrder(59, 75, 83);
+    
+    assertEquals("Die Bestellung(Anzahl: [59,75,83]) hat ein Gesamtgewicht von 0g und kostet 0PD.", to1.toString());
+    
+    to1.supplyOrder(new Anchovie(100, 200), BigDecimal.ONE);
+    assertEquals("Die Bestellung(Anzahl: [59,75,83]) hat ein Gesamtgewicht von 200g und kostet 1PD.", to1.toString());
+  }
+  
+  @Test
+  void isOrderFulfilledAmountOrder() {
+    TradeOrder to1 = new AmountOrder(1, 2, 3);
+    
+    assertFalse(to1.isOrderFulfilled());
+    
+    to1.supplyOrder(new Anchovie(17, 200), BigDecimal.ONE);
+    assertFalse(to1.isOrderFulfilled());
+    to1.supplyOrder(new Crustacean(200), BigDecimal.ONE);
+    to1.supplyOrder(new Crustacean(300), BigDecimal.ONE);
+    assertFalse(to1.isOrderFulfilled());
+    to1.supplyOrder(new Sardine(1, 100, 15), BigDecimal.ONE);
+    to1.supplyOrder(new Sardine(1, 100, 15), BigDecimal.ONE);
+    to1.supplyOrder(new Sardine(1, 100, 15), BigDecimal.ONE);
+    assertTrue(to1.isOrderFulfilled());
+    assertFalse(to1.supplyOrder(new Sardine(1, 100, 15), BigDecimal.ONE));
+    // see https://zulip.in.tum.de/#narrow/stream/846-PGdP-W06H01--.20Saleuine.20Grosshandel/topic/.E2.9C.94.20isOrderFulfilled.20AmountOrder/near/369739
+    assertTrue(to1.isOrderFulfilled());
+  }
+  
+  @Test
+  void supplyOrderAmountOrder() {
+    TradeOrder to1 = new AmountOrder(1, 2, 3);
+    
+    assertFalse(to1.supplyOrder(new PinguFood(1, 100), BigDecimal.ONE));
+    
+    assertTrue(to1.supplyOrder(new Anchovie(1, 100), BigDecimal.ONE));
+    assertFalse(to1.isOrderFulfilled());
+    
+    assertFalse(to1.supplyOrder(new Anchovie(1, 100), BigDecimal.ONE)); // Target already reached
+    
+    assertTrue(to1.supplyOrder(new Crustacean(100), BigDecimal.ONE));
+    assertTrue(to1.supplyOrder(new Crustacean(100), BigDecimal.ONE));
+    assertFalse(to1.isOrderFulfilled());
+    
+    assertFalse(to1.supplyOrder(new Crustacean(100), BigDecimal.ONE)); // Target already reached
+    
+    assertFalse(to1.supplyOrder(new Sardine(0, 100, 14), BigDecimal.ONE));
+    assertFalse(to1.supplyOrder(new Sardine(1, 100, 13), BigDecimal.ONE));
+    
+    assertTrue(to1.supplyOrder(new Sardine(1, 100, 14), BigDecimal.ONE));
+    assertTrue(to1.supplyOrder(new Sardine(1, 100, 14), BigDecimal.ONE));
+    assertTrue(to1.supplyOrder(new Sardine(1, 100, 14), BigDecimal.ONE));
+    assertTrue(to1.isOrderFulfilled());
+    
+    assertFalse(to1.supplyOrder(new Sardine(1, 100, 15), BigDecimal.ONE)); // Order is fulfilled
+  }
 }
